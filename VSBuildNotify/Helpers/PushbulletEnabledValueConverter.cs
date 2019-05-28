6f9969279683e2bd710f1d02f3faa66c425dadaa
Michał Kutzner <kucyk010@gmail.com>
2019-05-28 10:59:56 +0200

    Enabling Pushbullet related controls only when Notification type is set to Pushbullet

---
+++ VSBuildNotify/Helpers/PushbulletEnabledValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+using VSBuildNotify.Notifiers;
+
+namespace VSBuildNotify.Helpers
+{
+    public class PushbulletEnabledValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if ((NotifierType)value == NotifierType.PUSHBULLET)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
